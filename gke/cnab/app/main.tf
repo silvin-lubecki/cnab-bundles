ba2fbfdfb3f5acac54c2ca05e3523785a600498c
Silvin Lubecki <silvin.lubecki@docker.com>
2019-12-16 22:20:45 +0100

    Use terraform cloud to share state
    Signed-off-by: Silvin Lubecki <silvin.lubecki@docker.com>

--- gke/cnab/app/main.tf
+++ gke/cnab/app/main.tf
@@ -4,12 +4,37 @@ variable "project_id" {
 
 provider "google" {
   credentials = file("gcp-service-account.json")
-
   project = var.project_id
   region  = "us-central1"
   zone    = "us-central1-c"
 }
 
 resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+  name = "cnab-network"
+}
+
+resource "google_compute_address" "vm_static_ip" {
+  name = "cnab-static-ip"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "cnab-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+        nat_ip = google_compute_address.vm_static_ip.address
+    }
+  }
+}
+
+output "ip" {
+  value = google_compute_address.vm_static_ip.address
 }
